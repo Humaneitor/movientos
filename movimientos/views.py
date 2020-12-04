ac2f194ecfd48b2d89f628d25351c45787eaadf5
Ismael Bonilla <ibonilla@DESKTOP-KE9GC80.BAOPARTN.COM>
2020-12-04 11:35:53 +0100

    fin de la clase del jueves, por fin me salió

--- movimientos/views.py
+++ movimientos/views.py
@@ -1,6 +1,22 @@
 from movimientos import app
 from flask import render_template
+import csv
 
 @app.route('/')
-def listaMovimientos():
-    return render_template("movimientoslista.html", miTexto="Aquí pongo lo que quiero directamente como una función")
+def listaingresos():
+    fIngresos = open("movimientos/data/basededatos.csv", "r")
+    csvReader = csv.reader(fIngresos, delimiter=',', quotechar="\"")
+    ingresos = list(csvReader)
+
+    total = 0
+    for ingreso in ingresos:
+        total += float(ingreso[2])
+
+
+    return render_template("movimientoslista.html",datos=ingresos, total=total)
+
+@app.route('/creaalta')
+def nuevoIngreso():
+    return 'Ya el miércoles si eso te enseño el formulario'
+
+
