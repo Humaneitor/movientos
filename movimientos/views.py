6c026e586b9ad23106d50ba3a1a81d09442febdd
Ismael Bonilla <ibonilla@DESKTOP-KE9GC80.BAOPARTN.COM>
2020-12-13 17:53:31 +0100

    Funcional antes de los cambios de ramon del viernes

--- movimientos/views.py
+++ movimientos/views.py
@@ -1,22 +1,83 @@
 from movimientos import app
-from flask import render_template
+from flask import render_template, request, url_for, redirect
 import csv
+import sqlite3
+
 
 @app.route('/')
-def listaingresos():
-    fIngresos = open("movimientos/data/basededatos.csv", "r")
-    csvReader = csv.reader(fIngresos, delimiter=',', quotechar="\"")
-    ingresos = list(csvReader)
+def listaIngresos():
+    conn = sqlite3.connect('movimientos/data/basededatos.db')
+    c = conn.cursor()
+
+    c.execute('SELECT fecha, concepto, cantidad, id FROM movimientos;')
+
+    ingresos = c.fetchall()
 
     total = 0
     for ingreso in ingresos:
         total += float(ingreso[2])
 
+    conn.close()
 
     return render_template("movimientoslista.html",datos=ingresos, total=total)
 
-@app.route('/creaalta')
+@app.route('/creaalta', methods=['GET', 'POST'])
 def nuevoIngreso():
-    return 'Ya el miércoles si eso te enseño el formulario'
+    if request.method == 'POST':
+
+        conn = sqlite3.connect('movimientos/data/basededatos.db')
+        c = conn.cursor()
+
+        c.execute('INSERT INTO movimientos (cantidad, concepto, fecha) VALUES (?, ? ,? );', 
+                 (
+                    float(request.form.get('cantidad')),
+                    request.form.get('concepto'),
+                    request.form.get('fecha')
+                 )
+        )
+
+        conn.commit()
+        conn.close()
+    
+        return redirect(url_for('listaIngresos'))
+        
+
+
+    return render_template("alta.html")
+
+@app.route("/modifica/<id>", methods=['GET', 'POST'])
+def modifica(id):
+    conn = sqlite3.connect('movimientos/data/basededatos.db')
+    c = conn.cursor()
+
+    if request.method == 'GET':
+        
+        c.execute('SELECT fecha, concepto, cantidad id FROM movimientos where id = ?', (id,))
+        registro = c.fetchone()
+        conn.close()
+
+        return render_template("modifica.html", registro=registro)
+
+
+    else:
+        c.execute('UPDATE movimientos SET fecha = ?, concepto = ?, cantidad = ? WHERE id = ?',
+            (request.form.get('fecha'),
+            request.form.get('concepto'),
+            float(request.form.get('cantidad')),
+            id
+            )
+        )
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for('listaIngresos'))
+    
+
+
+
+
+
+
+
 
 
